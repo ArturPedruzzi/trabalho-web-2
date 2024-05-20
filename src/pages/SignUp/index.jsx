9a9e06fb107c2928ed248362d035867ddc4207b5
Artur Pedruzzi <arturpedruzzi023@gmail.com>
2024-05-19 22:42:24 -0300

    Commit verificando conteúdo final

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -96,7 +96,7 @@ export function SignUp() {
 
         <Link to="/">
           <FiArrowLeft />
-          Já tenho uma conta
+          Já possuo uma vaga
         </Link>
       </Form>
     </Container>
